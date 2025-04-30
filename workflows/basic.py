2d01e59011f137770f0dee70c45d6c2d271f5406
Arunselvan <arunselvan.n@gmail.com>
2025-04-30 08:40:14 -0300

    add module to call gpt and get structured response

---
+++ workflows/basic.py
@@ -0,0 +1,19 @@
+"""Module to explain how to call a gpt model"""
+import os
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+completion = client.chat.completions.create(
+    model="chatgpt-4o-latest",
+    messages=[
+        {"role": "system", "content": "You're a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a limerick about the Python programming language.",
+        },
+    ]
+)
+
+response = completion.choices[0].message.content
+print(response)
---
+++ workflows/structured.py
@@ -0,0 +1,29 @@
+"""Module to get response in structured format from a gpt model"""
+import os
+from openai import OpenAI
+from pydantic import BaseModel
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+class CalendarEvent(BaseModel):
+    """Event Response schema"""
+    name: str
+    date: str
+    participants: list[str]
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "system", "content": "Extract the event information."},
+        {
+            "role": "user",
+            "content": "Alice and Bob are going to a science fair on Friday.",
+        },
+    ],
+    response_format=CalendarEvent,
+)
+
+event = completion.choices[0].message.parsed
+print(event.name)
+print(event.date)
+print(event.participants)
